04b735b3bbc8e53fcae72d31511e17fefae522d8
Michal Nowak <michalnowak.contact@gmail.com>
2016-02-02 17:58:39 +0100

    Timing change

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 
 $("#lemonade").animate({ 
 	opacity: "1", 
-	bottom: "0px" }, 1000);
+	bottom: "0px" }, 800);
 
 $(".animate-delay").delay(300).animate({ 
 	opacity: "1",
-	bottom: "0px" }, 1000);
+	bottom: "0px" }, 800);
