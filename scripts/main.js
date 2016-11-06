7bd1f9152b8bf8926efb0416afce20483debb7da
luckymichaln <michalnowak.contact@gmail.com>
2016-11-06 20:59:19 +0100

    #1: add first step of jquery validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,38 @@
+var $icon = $('svg');
+var $input = $('input');
+var $hint = $('.hint');
+var $submit = $('.btn-submit');
 
-$(".title").addClass("animation");
+$input.on('focus', function(e) {
+    var target = $(e.target);
+    var parent = target.closest('fieldset');
+    parent.find($hint).addClass('visible');
+});
 
-var delay = setTimeout(function () {
-    $(".anim-delay").addClass("animation");
-}, 400);
+$input.on('blur', function(e) {
+    var target = $(e.target);
+    var parent = target.closest('fieldset');
+    var visibleHint = parent.find($hint);
+
+    if (target.val() > 0) {
+        visibleHint.addClass('visible');
+
+    } else if (target.val() <= 0) {
+        visibleHint.removeClass('visible');
+    }
+
+});
+
+$submit.on('click', function(e) {
+    var target = $(e.target);
+    var parent = target.closest('form');
+    var inputs = parent.find($input);
+
+    // if (inputs.hasClass('accept')) {
+    // }
+
+    // else {
+        e.preventDefault();
+        // target.removeClass('ready-to-fire');
+    // }
+});
